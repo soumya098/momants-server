e438fe1779c5b378070fc1fe1af5ce62b957c5e9
soumya098 <soumyarj904@gmail.com>
2022-04-09 23:40:23 +0530

    setup and install dependencies

---
+++ index.js
@@ -0,0 +1,6 @@
+// const express = require('express');  for older version of Node
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import cors from "cors";
+
