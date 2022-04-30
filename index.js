c2638a980b793504dc2c7551f6efa37fb865b9c8
soumya098 <soumyarj904@gmail.com>
2022-04-30 21:00:02 +0530

    minor change

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ import dotenv from "dotenv";
 
 //app config
 const app = express();
-const PORT = process.env.PORT || 5000;
 dotenv.config();
+const PORT = process.env.PORT || 5000;
 
 //middlewares
 //app.use(express.json());
